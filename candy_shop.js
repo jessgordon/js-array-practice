7d975a82255fe5be246e4213fffaa8d6bcbff938
Jessica Gordon <ketchesj@gmail.com>
2022-01-05 14:32:17 +0000

    step 14 using map with arrays to generate personalised messages

--- candy_shop.js
+++ candy_shop.js
@@ -1,5 +1,13 @@
-// add number to a batch of ids if there are less than 5 ids already in the array
+const numbers = [
+  '1763687364',
+  '4763687363',
+  '7867867862',
+  'aaaaaaaabbbbbbbcccccdddddddd' 
+];
 
+const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
+
+// add number to a batch of ids if there are less than 5 ids already in the array
 const addToBatch = (array, number) => {
   if (array.length < 5) {
     return array.concat(number);
@@ -7,10 +15,7 @@ const addToBatch = (array, number) => {
     return array
 }
 
-console.log(addToBatch([1, 2, 3], 5));
-
 // filter out corrupt data from a list of customer phone numbers by length
-
 const checkLength = (number) => {
   return number.length <= 10;
 }
@@ -19,13 +24,20 @@ const filterLongNumbers = (array_of_numbers) => {
   return array_of_numbers.filter(checkLength);
 }
 
-const numbers = [
-  '1763687364',
-  '4763687363',
-  '7867867862',
-  'aaaaaaaabbbbbbbcccccdddddddd' // this element should be filtered
-];
+// personalised text message using map function
+const generateMessages = (names_array) => {
+  return names_array.map(name => {
+    return `Hi ${name}! 50% off our best candies for you today!`;
+  });
+}
 
-console.log(filterLongNumbers(numbers));
-console.log(filterLongNumbers(['4763687363', '7867867862']));
-console.log(filterLongNumbers([]));
+
+// Testing functions
+
+// console.log(addToBatch([1, 2, 3], 5));
+
+// console.log(filterLongNumbers(numbers));
+// console.log(filterLongNumbers(['4763687363', '7867867862']));
+// console.log(filterLongNumbers([]));
+
+// console.log(generateMessages(names));
